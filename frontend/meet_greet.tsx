cf624117b740dcfd2c23a7cc2fdc844600d089da
LeoC <leo.cheng92@gmail.com>
2022-03-22 21:27:29 -0400

    set up project skeleton

---
+++ frontend/meet_greet.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootEl = document.getElementById("root")
+  ReactDOM.render(
+    <h1>React is up and running!</h1>,
+    rootEl
+  )
+})
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/meet_greet.tsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*', '.tsx', '.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react', '@babel/typescript']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
