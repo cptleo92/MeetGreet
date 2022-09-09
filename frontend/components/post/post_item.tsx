3987ceb645cdd6fa737840beba568761dd3bb35a
LeoC <leo.cheng92@gmail.com>
2022-09-08 20:01:52 -0400

    add basic delete post button

--- frontend/components/post/post_item.tsx
+++ frontend/components/post/post_item.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { RootState } from "../../store/store";
 import { Post, UserName } from "../../types/types";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 import { useUser } from "../../util/hooks";
+import { deletePost } from "../../actions/users_actions";
 
 const PostItem = ({ post }: { post: Post }) => {
   // taking user info from attendees which should already have everything
@@ -14,12 +15,16 @@ const PostItem = ({ post }: { post: Post }) => {
   const currentUser = useUser();
   const belongsToUser = postAuthor.id === currentUser.id;
 
+  const dispatch = useDispatch();
+
   const getDaysAgo = () => {
     return moment.utc(post.created_at).local().startOf('seconds').fromNow()
   }
 
-  const deletePost = () => {
-
+  const removePost = () => {
+    if (belongsToUser) {
+      dispatch(deletePost(post))
+    }
   }
 
   return (
@@ -38,14 +43,14 @@ const PostItem = ({ post }: { post: Post }) => {
        { belongsToUser && 
         <div className="content-footer">
           <p>{getDaysAgo()}</p>          
-          <div className="post-delete" onClick={deletePost}>
+          <div className="post-delete" onClick={removePost}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 {/* <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
                 <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
             </div>
          </div>
         }
-        
+
       </div>
     </div>
   );
